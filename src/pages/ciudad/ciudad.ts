032e6059a4baeccae9fd5db370fa8728c3d21483
wilnertm <winlly95@hotmail.com>
2018-11-06 09:26:27 -0500

    Cambios en html de pages usuario y ciudad

--- src/pages/ciudad/ciudad.ts
+++ src/pages/ciudad/ciudad.ts
@@ -12,11 +12,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CiudadPage {
 
+  departamentoPage = "departamento"
+  usuarioPage = "usuario"
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CiudadPage');
+  }
+
+  goto(page){
+    switch (page) {
+      case "departamento":
+        this.navCtrl.push("ciudad")
+        break;
+
+      case "usuario":
+        this.navCtrl.push("usuario")
+        break;
+    
+      default:
+        break;
+    }
   }
 
 }
